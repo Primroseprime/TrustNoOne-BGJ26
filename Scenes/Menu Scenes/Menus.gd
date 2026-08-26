772356240346f4a53855baf9a6bcfe32dfb91304
CalGGF <calggffman@gmail.com>
2026-08-25 23:39:56 -0400

    Updated Buttons
    Any simple button is now a single scene instead of using 2 nodes and recoding every button manually.

--- Scenes/Menu Scenes/Menus.gd
+++ Scenes/Menu Scenes/Menus.gd
@@ -1,21 +1,36 @@
 extends Node2D
 
-@onready var Credits_Menu_Scene = preload("res://Scenes/Menu Scenes/Credits Menu Scene/credits_menu.tscn").instantiate()
+# Get Scenes
+@onready var Credits_Scene = preload("res://Scenes/Menu Scenes/Credits Menu Scene/credits_menu.tscn")
 
-# Signal Connect
+# Connect Buttons
 func _ready() -> void:
-	$"Main Menu/Credits Button/Credits area".connect("Show_Credits_Scene", Show_Credits)
+	$"Main Menu/Play Button/Button Area".connect("Clicked", Play)
+	$"Main Menu/Options Button/Button Area".connect("Clicked", Options)
+	$"Main Menu/Credits Button/Button Area".connect("Clicked", Credits)
+	$"Main Menu/Quit Button/Button Area".connect("Clicked", Quit)
 
-# Var
-var Credits_Opened := false
+#Clicked Play -> Start Game
+func Play():
+	pass
 
-# Show Credit Scene
-func Show_Credits():
-	add_child(Credits_Menu_Scene)
-	Credits_Opened = true
+# Clicked Options -> Open Options Scene
+func Options():
+	pass
 
-# Remove Credit Scene
+# Clicked Credits -> Open Credit Scene
+var Credits_Scene_Instantiated
+func Credits():
+	Credits_Scene_Instantiated = Credits_Scene.instantiate()
+	add_child(Credits_Scene_Instantiated)
+	Credit_Menu_Open = true
+	
+# Close Credit Scene if open and pressed 'ESC'
+var Credit_Menu_Open := false
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ESC") and Credits_Opened:
-		remove_child(Credits_Menu_Scene)
-		Credits_Opened = false
+	if Input.is_action_just_pressed("ESC"):
+		Credits_Scene_Instantiated.queue_free()
+
+# Clicked Quit -> Quit
+func Quit():
+	get_tree().quit()
--- Scenes/Universal Scenes/button_script.gd
+++ Scenes/Universal Scenes/button_script.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
-# Var
+# Checking is mouse is over button
 var is_hovering := false
 
-# Get if hovering
 func _on_mouse_entered() -> void:
 	is_hovering = true
 func _on_mouse_exited() -> void:
 	is_hovering = false
 
-# When Clicked Quit -> Quit Game
+# If clicked -> send signal
+signal Clicked
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Left Click") and is_hovering:
-		get_tree().quit()
+		emit_signal("Clicked")
